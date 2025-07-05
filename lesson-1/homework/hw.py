079e2268e62c0c8dd880731d900c03de1acf2540
odinabkh1721 <bahodirovnaodina12@gmail.com>
2025-07-05 13:55:38 +0500

    Create hw.py

---
+++ lesson-1/homework/hw.py
@@ -0,0 +1,32 @@
+
+# Given a side of square. Find its perimeter and area.
+
+a=float(input("enter a side of square: "))
+p=a**2
+print(float(p))
+
+
+
+# Given diameter of circle. Find its length.
+
+pi=3.14
+diameter = float(input("Enter the diameter of the circle: "))
+length = pi * diameter
+
+print("Diametr:", diameter, "| Doira uzunligi:", length)
+
+
+# Given two numbers a and b. Find their mean.
+
+a = 8
+b = 6
+print((a + b) / 2)
+
+
+# Given two numbers a and b. Find their sum, product and square of each number.
+
+a = 7
+b = 2
+print (a+b)
+print( a*b)
+print (a**2, b**2)
